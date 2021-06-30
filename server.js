461db4118071d5ae40725a9a149384c632acc0d6
nasernikzad <n.nikzad@pomtech.com>
2021-06-30 10:08:08 +0430

    result added

--- server.js
+++ server.js
@@ -19,6 +19,15 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/api/whoami',function(req,res){
+
+  let outPut = { IP : req.ip
+    , language : req.headers["accept-language"], software : req.headers["user-agent"] };
+
+  res.json(outPut);
+
+});
+
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
